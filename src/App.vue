a8ad5c3510a387bb7651ada4a5c4872be58118c8
Ahmad Ali <ahmad12893@hotmail.com>
2023-07-16 16:48:48 +0100

    completed all tasks except 6

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
 <script setup>
-import Poses from './components/pose_test.vue'
+import Poses from './components/pose_test.vue';
+import { proto } from './components/proto_demo.mjs';
+
+//console.log(proto);
+//shows proto array of objects
+//console.log(proto[0].properties);
+//shows properties of first object in proto array, so to get all of them, use a for loop to iterate through the array and get the properties of each object
+//have to use v-for directive to iterate through the array and get the properties of each object
+//source to revisit: https://www.tutorialspoint.com/how-to-write-and-use-for-loop-in-vue-js#:~:text=Looping%20through%20an%20Array,-Example&text=We%20use%20the%20v%2Dfor,value%20of%20each%20named%20item.&text=This%20is%20the%20main%20file,available%20inside%20the%20src%20folder.
 </script>
 
 <template>
-
-  <div style="margin: 10px;">
+  <div style="margin: 10px">
     Tech test - Tasks:
     <div>
       <div class="task">
-        1. display all values given in the 'properties' object for each entry, use your judgment on how to display them
+        1. display all values given in the 'properties' object for each entry,
+        use your judgment on how to display them
+      </div>
+      <div>
+        <h3>Properties</h3>
+        <ul v-for="(object, idx) in proto" :key="object.properties.sub">
+          <h4>Tool {{ idx + 1 }}</h4>
+          <p>
+            The {{ object.properties.finer }}
+            {{ object.properties.broader }} has a length of
+            {{ object.properties.length }} inches,
+            <template v-if="object.properties.characteristic">
+              with a {{ object.properties.characteristic }} characteristic,
+            </template>
+
+            <template v-if="object.properties.speciality">
+              and the specialities where it's use include:
+              <br />
+
+              <li v-for="thing in object.properties.speciality" :key="thing">
+                {{ thing }}
+              </li>
+            </template>
+            <br />
+            <template v-if="object.properties.sub">
+              This tools sub-category is {{ object.properties.sub }}
+            </template>
+          </p>
+        </ul>
       </div>
       <div class="task">
         2. comment on each function in 'pose_test.vue' and explain what it does
         <br />(extra credit for commenting on the other functions in the file)
+        <h3>Done, check comments</h3>
       </div>
       <div class="task">
         3. contain the contents of each entry within the blue container
+
+        <h3>
+          Done, put all the info inside the entries of the proto array in a blue
+          container
+        </h3>
       </div>
       <div class="task">
-        4. convert the pose buttons to a dropdown menu using the vue composition api bindings to set the face of the cube (space-out contents of each entry as you see fit)
+        4. convert the pose buttons to a dropdown menu using the vue composition
+        api bindings to set the face of the cube (space-out contents of each
+        entry as you see fit)
       </div>
       <div class="task">
-        5. create filter checkboxes for each of the poses (face up, face down, right, left, vertical up, vertical down) and filter the list of entries based on the selected options
+        5. create filter checkboxes for each of the poses (face up, face down,
+        right, left, vertical up, vertical down) and filter the list of entries
+        based on the selected options
       </div>
       <div class="task">
-        6. change the background color of each blue container on hover by using the vue action and binding the hover sass class
+        6. change the background color of each blue container on hover by using
+        the vue action and binding the hover sass class
         <br />(you can change all the colours as you please)
       </div>
     </div>
@@ -34,7 +80,6 @@ import Poses from './components/pose_test.vue'
   </div>
 
   <Poses />
-
 </template>
 
 <style scoped lang="sass">
--- src/components/pose_test.vue
+++ src/components/pose_test.vue
@@ -1,94 +1,163 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { proto } from './proto_demo.mjs'
+import { ref, onMounted } from 'vue';
+import { proto } from './proto_demo.mjs';
 
-const faces = ['face_up', 'face_down', 'right', 'left', 'vertical_up', 'vertical_down']
-
+const faces = [
+  'face_up',
+  'face_down',
+  'right',
+  'left',
+  'vertical_up',
+  'vertical_down',
+];
+//faces array contains the 6 possible poses
 let instrument_settings = (i) => {
-  let n = 1
-  let calc = x => n > 0 ? x - 1 : x
-
+  let n = 1;
+  //n is set to 1
+  let calc = (x) => (n > 0 ? x - 1 : x);
+  //calc is a function that takes in x and returns x - 1 if n is greater than 0, otherwise it returns x
   let [f, s, v] = [
-    calc(i.poses.faces), 
-    calc(i.poses.sides), 
-    calc(i.poses.vertical)
-  ]
-  
-  let face_option = f > 2 ? [faces[1]] 
-  : f > 1 ? [faces[0]] 
-  : f > 0 ? [faces[0], faces[1]] 
-  : []
-  let sides = s > 2 ? [faces[2]] 
-  : s > 1 ? [faces[3]] 
-  : s > 0 ? [faces[3], faces[2]] 
-  : []
-  let vertical = v > 2 ? [faces[5]]
-  : v > 1 ? [faces[4]] 
-  : v > 0 ? [faces[4], faces[5]] 
-  : []
+    calc(i.poses.faces),
+    calc(i.poses.sides),
+    calc(i.poses.vertical),
+  ];
+  // f, s, and v are set to the results of calc with the corresponding pose values from the proto array of objects
 
-  return [...face_option, ...sides, ...vertical]
-}
+  let face_option =
+    f > 2 ? [faces[1]] : f > 1 ? [faces[0]] : f > 0 ? [faces[0], faces[1]] : [];
+  // if f is more than 2, face_option will be the second element in the faces array, if it (f) is greather than 1, face_option will be the first element (faces[0]), if f is greater than 0, both elements of the array will be shown, otherwise face_option will be an empty array.
 
-const cubes = ref([])
-const hl = ref([])
+  let sides =
+    s > 2 ? [faces[2]] : s > 1 ? [faces[3]] : s > 0 ? [faces[3], faces[2]] : [];
 
+  //if s is more than 2, the third element in the faces array will be shown, else if s is greater than 1, the fourth element will be shown, if s is more than 0, both the third and fourth elements will be shown, otherwise sides will be a empty array.
+
+  let vertical =
+    v > 2 ? [faces[5]] : v > 1 ? [faces[4]] : v > 0 ? [faces[4], faces[5]] : [];
+
+  //doing the same thing as the previous 2, where if v is greater than 2, the sixth element of the faces array will be shown, if v is greater than 1, the fifth element will be shown and if it is greater than 0 then both the fifth and sixth element will be shown, otherwise vertical will be a empty array.
+
+  return [...face_option, ...sides, ...vertical];
+
+  // this will show the arrays above in order of face_option, sides and vertical, so basically the options are face_option is empty, sides and vertical will show, sides is empty, face_option and vertical will show and vertical is empty, face_option and sides will show and if all of them are somehow empty then nothing will be shown, otherwise, all 3 will be shown.
+};
+
+const cubes = ref([]);
+const hl = ref([]);
+// both  is referencing to an empty array
+
+//store selected face of each item in a state
+const faceSelectedByUser = ref([]);
+
+//store the selected poses in a state
+const selectedPoses = ref([]);
+
+/*
 const chng_face = (i, face) => {
-  let c = cubes.value.filter(ii => ii[0] != i)
-  let h = hl.value.filter(ii => ii[0] != i)
-  c.push([i, `show-${face}`])
-  h.push([i, face])
-  cubes.value = c
-  hl.value = h
-}
+  let c = cubes.value.filter((ii) => ii[0] != i);
+  let h = hl.value.filter((ii) => ii[0] != i);
+  c.push([i, `show-${face}`]);
+  h.push([i, face]);
+  cubes.value = c;
+  hl.value = h;
+};
+*/
 
-const getCube = i => {
-  let c = cubes.value.filter(ii => ii[0] == i)
-  return c.length > 0 ? c[0][1] : null
-}
+const chng_face = (i, event) => {
+  faceSelectedByUser.value[i] = event.target.value;
+};
+
+// the face is the face of the cube and i is the index, these are taken as arguements for the function,c is set to the empty cubes array (initial state) from before and h to the hl array, both are then filtered to show the index that is not equal to arguemnt i (ii => ii[0] != i), then pushed into the c and h arrays, which cubes and hl is set to (similar to setCube, setHl).
+
+//changed this to get the value of the selected side from the dropdown
+const getCube = (i) => {
+  const selected = faceSelectedByUser.value[i];
+  return selected ? `show-${selected}` : null;
+};
+
+// this functions gets the cube, where c is filtered for the arguemented index, (if the arguement index is the same as the index in the array, then show it), then if this length of the cube is greater than 0 (c.length > 0), show the first element other wise show nothing (null)
 
 const getHL = (i, face) => {
-  let h = hl.value.filter(ii => ii[0] == i)
-  return h.length > 0 && h[0][1] == face ? true : false
-}
+  let h = hl.value.filter((ii) => ii[0] == i);
+  return (
+    h.length > 0 &&
+    h[0][1] == face &&
+    faceSelectedByUser.value[i]?.includes(face)
+  );
+};
+
+// this function gets the highlights, where i is the index and face is the face of the cube, h is set to the aforementioned hl array and does the same thing as before where it filters hl for the arguement index, and if the length of h is greater AND the first element of h is the face, then it will return true, otherwis it will return false.
+
+
+//REDO THIS
+
+const filterEntry = (entry) => {
+  if (selectedPoses.value.length === 0) {
+    return true; // Display all entries if no pose options are selected
+  } else {
+    const poses = instrument_settings(entry.settings);
+    return selectedPoses.value.every((pose) => poses.includes(pose));
+  }
+
+};
 
 </script>
 
 <template>
-
+  <td>
+    <div>
+      <label v-for="pose in faces" :key="pose">
+        <input type="checkbox" :value="pose" v-model="selectedPoses" />
+        {{ pose }}
+      </label>
+    </div>
+  </td>
+  <br />
+  <br />
   <table class="list">
     <th>face</th>
     <th>options</th>
     <th>result</th>
 
     <template v-for="(item, idx) of proto">
-      
+    <tr v-if="filterEntry(item)">
       <tr class="title space">
         <td colspan="3">{{ '|   ' }}</td>
       </tr>
-      
+
       <tr class="title">
         <td colspan="3">{{ item.scalpel_id }}</td>
       </tr>
-      
+
       <tr class="v_up">
         <td>{{ idx }}</td>
         <td class="code">
           poses: {{ item.settings.poses }}
+          <br />
+          <br />
+          time: {{ item.time }}
+          <br />
+          <br />
+          properties: {{ item.properties }}
+          <br />
+          <br />
+          Admin: {{ item.admin }}
         </td>
         <td>
           <div v-for="res of instrument_settings(item.settings)">{{ res }}</div>
         </td>
       </tr>
-      
-      <tr>
 
+      <tr>
         <td>
           <div class="scene">
             <div :class="`cube ${getCube(idx)}`">
-              <div 
-              v-for="pose in faces" 
-              :class="`cube__face cube__face--${pose}${getHL(idx, pose) ? ' highlight' : ''}`">
+              <div
+                v-for="pose in faces"
+                :class="`cube__face cube__face--${pose}${
+                  getHL(idx, pose) ? ' highlight' : ''
+                }`"
+              >
                 {{ pose.split('_').join(' ') }}
               </div>
             </div>
@@ -96,33 +165,40 @@ const getHL = (i, face) => {
         </td>
 
         <td>
-
-          <div style="display: flex; flex-direction: row;">
-            <div v-for="face in instrument_settings(item.settings)" class="scene2">
+          <div style="display: flex; flex-direction: row">
+            <div
+              v-for="face in instrument_settings(item.settings)"
+              class="scene2"
+            >
               <div :class="`cube2 show-${face}`">
-                <div 
-                v-for="pose in faces" 
-                :class="`cube2__face cube2__face--${pose}`">
+                <div
+                  v-for="pose in faces"
+                  :class="`cube2__face cube2__face--${pose}`"
+                >
                   {{ pose == face ? String(pose).split('_').join(' ') : null }}
                 </div>
               </div>
             </div>
           </div>
 
-          <div style="display: flex; flex-direction: row;">
-            <button
-            v-for="face in faces"
-            @click="chng_face(idx, face)">
-              {{ face }}
-            </button>
+          <div style="display: flex; flex-direction: row">
+            <select
+              v-model="faceSelectedByUser[idx]"
+              @change="chng_face(idx, $event)"
+            >
+              <option
+                v-for="face in instrument_settings(item.settings)"
+                :value="face"
+              >
+                {{ face }}
+              </option>
+            </select>
           </div>
-
         </td>
       </tr>
-    
+      </tr>
     </template>
   </table>
-
 </template>
 
 <style scoped lang="sass">
@@ -170,6 +246,10 @@ tr
 td
   padding: 10px 0 0 10px
 
+.list td:hover
+  background: red
+
+
 .col
   display: flex
   flex-direction: column
@@ -177,5 +257,4 @@ td
 
 .active
   background-color: #565ed8
-
 </style>
